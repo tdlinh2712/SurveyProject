5d6ac27a7fc1c9a93acac8e9724e8d3509ba2411
tdlinh2712 <tdlinh2712@gmail.com>
2020-08-04 21:30:21 +1000

    add client side

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {} , applyMiddleware());
+const store = createStore(reducers, {} , applyMiddleware(reduxThunk));
 
 
 ReactDOM.render(
     <Provider store = {store}> <App/> </Provider>, 
     document.querySelector('#root')
 );
+
+console.log('Stripe key is',process.env.REACT_APP_STRIPE_KEY);
+console.log('node environment is',process.env.NODE_ENV);
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +11,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
     cookieSession({
@@ -21,6 +25,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+//routing in production
+if(process.env.NODE_ENV=== 'production') {
+    //express will serve up production assets like our main.js or main.css files
+    app.use(express.static('client/build')); //if express doesnt recognize the route, try to look into client/build to see if there's anything that matches
+
+    //express will serve up index.html file if it doesnt recognize the route
+    //for if there is nothing in routes folder and client/build => give index.html
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
